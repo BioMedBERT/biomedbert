adb5767f0692ab1d2b6f1b21bff8ff3a268c028c
Ekaba Bisong <dvdbisong@gmail.com>
2020-04-08 22:17:26 -0600

    generate pre-trained data

--- biomedbert.py
+++ biomedbert.py
@@ -8,7 +8,8 @@ Usage:
   biomedbert gcp vm stop <vm-instance>
   biomedbert gcp vm notebook <vm-instance>
   biomedbert gcp vm connect <vm-instance>
-  biomedbert code train vocab <data> <prefix>
+  biomedbert code train vocab <data_path> <prefix>
+  biomedbert code make pretrain data <pre_trained_dir> <voc_filename> <num_shard> <data_path>
   biomedbert -h | --help
   biomedbert --version
 
@@ -21,7 +22,7 @@ from __future__ import unicode_literals, print_function
 import configparser
 from docopt import docopt
 from gcp.gcp_helpers import set_gcp_project, start_vm, stop_vm, launch_notebook, connect_vm
-from code_.modules import train_vocabulary
+from biomedbert.modules import train_vocabulary, generate_pre_trained_data
 
 __version__ = "0.1.0"
 __author__ = "AI vs COVID-19 Team"
@@ -33,8 +34,12 @@ def code_commands(args: dict):
 
     # train vocab
     if args['code'] and args['train'] and args['vocab']:
-        if args['<data>'] and args['<prefix>']:
-            train_vocabulary(args['<data>'], args['<prefix>'])
+        train_vocabulary(args['<data_path>'], args['<prefix>'])
+
+    # generate pre-trained dataset
+    if args['code'] and args['make'] and args['pretrain'] and args['data']:
+        generate_pre_trained_data(args['<pre_trained_dir>'], args['<voc_filename>'],
+                                  args['<num_shard>'], args['<data_path>'])
 
 
 def gcp_commands(args: dict):
