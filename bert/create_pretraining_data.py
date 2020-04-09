3da0475ce7e5f23ffe0502a4307b5754d9fd89ed
Ekaba Bisong <dvdbisong@gmail.com>
2020-04-09 12:51:38 -0600

    fix bert code

--- bert/create_pretraining_data.py
+++ bert/create_pretraining_data.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+sys.path.append("bert")
+
 import collections
 import random
 # import tokenization
