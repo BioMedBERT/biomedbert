5349861c9a64ee26edf95bbfb6549d5fa7f9116b
Ekaba Bisong <dvdbisong@gmail.com>
2020-04-24 10:53:27 -0600

    squad run v1 and v2

--- bash/run_squad_v2.sh
+++ bash/run_squad_v2.sh
@@ -15,7 +15,7 @@ python3 bert/run_squad.py \
   --train_file=$SQUAD_DIR/train-v2.0.json \
   --do_predict=True \
   --predict_file=$SQUAD_DIR/dev-v2.0.json \
-  --train_batch_size=512 \
+  --train_batch_size=256 \
   --learning_rate=3e-5 \
   --num_train_epochs=2.0 \
   --max_seq_length=384 \
