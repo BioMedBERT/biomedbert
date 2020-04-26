d1a0fa6d20696d02ec8ac6227971a34d26f62a2d
Shweta Bhatt <shwetabhatt@Shwetas-MacBook-Pro-2.local>
2020-04-27 00:25:41 +0530

    Add: re_modules.py

---
+++ biomedbert_impl/re_modules.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from subprocess import call, CalledProcessError
+
+
+def run_re_gad_1(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 1"""
+    try:
+        call(['bash', './bash/run_re_gad_1.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 1')
+
+
+def run_re_gad_2(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 2"""
+    try:
+        call(['bash', './bash/run_re_gad_2.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 2')
+
+def run_re_gad_3(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 3"""
+    try:
+        call(['bash', './bash/run_re_gad_3.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 3')
+
+def run_re_gad_4(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 4"""
+    try:
+        call(['bash', './bash/run_re_gad_4.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 4')
+
+def run_re_gad_5(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 5"""
+    try:
+        call(['bash', './bash/run_re_gad_5.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 5')
+
+def run_re_gad_6(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad_7"""
+    try:
+        call(['bash', './bash/run_re_gad_6.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 6')
+
+def run_re_gad_7(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 7"""
+    try:
+        call(['bash', './bash/run_re_gad_7.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 7')
+
+def run_re_gad_8(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 8"""
+    try:
+        call(['bash', './bash/run_re_gad_8.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 8')
+
+def run_re_gad_9(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 9"""
+    try:
+        call(['bash', './bash/run_re_gad_9.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 9')
+
+def run_re_gad_10(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re gad 10"""
+    try:
+        call(['bash', './bash/run_re_gad_10.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE GAD 10')
+
+def run_re_euadr_1(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 1"""
+    try:
+        call(['bash', './bash/run_re_euadr_1.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 1')
+
+def run_re_euadr_2(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 2"""
+    try:
+        call(['bash', './bash/run_re_euadr_2.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 2')
+
+def run_re_euadr_3(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 3"""
+    try:
+        call(['bash', './bash/run_re_euadr_3.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 3')
+
+def run_re_euadr_4(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 4"""
+    try:
+        call(['bash', './bash/run_re_euadr_4.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 4')
+
+def run_re_euadr_5(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 5"""
+    try:
+        call(['bash', './bash/run_re_euadr_5.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 5')
+
+def run_re_euadr_6(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 6"""
+    try:
+        call(['bash', './bash/run_re_euadr_6.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 6')
+
+def run_re_euadr_7(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 7"""
+    try:
+        call(['bash', './bash/run_re_euadr_7.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 7')
+
+def run_re_euadr_8(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 8"""
+    try:
+        call(['bash', './bash/run_re_euadr_8.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 8')
+
+def run_re_euadr_9(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 9"""
+    try:
+        call(['bash', './bash/run_re_euadr_9.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 9')
+
+def run_re_euadr_10(biomedbert_gcs_dir: str, biomedbert_model_type: str):
+    """run re euadr 10"""
+    try:
+        call(['bash', './bash/run_re_euadr_10.sh', biomedbert_gcs_dir, biomedbert_model_type])
+    except CalledProcessError:
+        print('Cannot run RE euadr 10')
+
+
