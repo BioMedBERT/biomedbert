fe371ca7a2a5d1592bde6d0e59e52cd348e38134
Ekaba Bisong <dvdbisong@gmail.com>
2020-05-21 03:21:56 -0600

    update bioasq script

--- biomedbert_impl/bioasq_modules.py
+++ biomedbert_impl/bioasq_modules.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from invoke import run, exceptions
 
 
@@ -18,7 +19,7 @@ def fine_tune_bioasq(train_file: str, predict_file: str, model_dir: str, init_ch
     try:
         # TODO: parameterize bioasq dataset on gcs
         run('python3 biobert/run_qa.py  --vocab_file={}/{}   '
-            '--bert_config_file={}/bert_config.json   '
+            '--bert_config_file={}/bert_config.json   --predict_batch_size=128'
             '--init_checkpoint={}/{}   --do_train=true --do_predict=true  '
             '--max_seq_length=384   --train_batch_size=128   --learning_rate=5e-6   '
             '--doc_stride=128   --num_train_epochs=5.0   --do_lower_case=False   '
@@ -31,3 +32,28 @@ def fine_tune_bioasq(train_file: str, predict_file: str, model_dir: str, init_ch
             use_tpu, tpu_name, tpu_zone, gcp_project))
     except exceptions.UnexpectedExit:
         print('Cannot fine tune BioASQ - {}'.format(train_file))
+
+
+def evaluate_bioasq(model_dir: str, train_file: str):
+    """evaluate bioasq"""
+
+    output_dir = '{}/BioASQ_outputs/{}/'.format(model_dir, train_file.split('.')[0])
+    # convert results to BioASQ JSON format
+    try:
+        run('python3 biobert/biocodes/transform_nbset2bioasqform.py   '
+            '--nbest_path={}/nbest_predictions.json --output_path={}'.format(output_dir, output_dir))
+    except exceptions.UnexpectedExit:
+        print('Cannot convert results to BioASQ JSON format')
+        sys.exit(1)
+
+    # run BioAsq evaluation script
+    try:
+        run('git clone https://github.com/BioASQ/Evaluation-Measures.git')
+        run('cd Evaluation-Measures')
+        run('java -Xmx10G '
+            '-cp $CLASSPATH:./flat/BioASQEvaluation/dist/BioASQEvaluation.jar evaluation.EvaluatorTask1b '
+            '-phaseB -e 5 gs://ekaba-assets/datasets/QA/BioASQ/4B1_golden.json '
+            '{}/BioASQform_BioASQ-answer.json'.format(output_dir))
+    except exceptions.UnexpectedExit:
+        print('Cannot evaluate BioASQ')
+        sys.exit(1)
