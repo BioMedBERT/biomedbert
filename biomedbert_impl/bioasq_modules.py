56f93e8c6297b140d6cb72105a7c7cef264bd43c
Ekaba Bisong <dvdbisong@gmail.com>
2020-05-21 02:50:42 -0600

    update bioasq script

--- biomedbert_impl/bioasq_modules.py
+++ biomedbert_impl/bioasq_modules.py
@@ -22,10 +22,10 @@ def fine_tune_bioasq(train_file: str, predict_file: str, model_dir: str, init_ch
             '--doc_stride=128   --num_train_epochs=5.0   --do_lower_case=False   '
             '--train_file=gs://ekaba-assets/datasets/QA/BioASQ/{}   '
             '--predict_file=gs://ekaba-assets/datasets/QA/BioASQ/{}   '
-            '--output_dir={}/QA_outputs/{}/  --num_tpu_cores=128   --use_tpu={}   '
+            '--output_dir={}/BioASQ_outputs/{}/  --num_tpu_cores=128   --use_tpu={}   '
             '--tpu_name={}   --tpu_zone={}  --gcp_project={}'.format(
             model_dir, vocab_file, model_dir, model_dir, init_checkpoint,
             train_file, predict_file, model_dir, train_file.split('.')[0],
             use_tpu, tpu_name, tpu_zone, gcp_project))
     except exceptions.UnexpectedExit:
-        print('Cannot fine tune QA - {}'.format(train_file))
+        print('Cannot fine tune BioASQ - {}'.format(train_file))
