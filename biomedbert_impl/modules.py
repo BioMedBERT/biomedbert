56148d0c4251ee9a43a67e188da354b1076267bc
Ekaba Bisong <dvdbisong@gmail.com>
2020-05-22 17:56:22 -0600

    update biomedbert scratch training

--- biomedbert_impl/modules.py
+++ biomedbert_impl/modules.py
@@ -19,7 +19,8 @@ log = logging.getLogger('biomedbert')
 log.setLevel(logging.INFO)
 
 
-def train_biomedbert(model_type: str, model_dir: str, pretraining_dir: str, bucket_name: str, tpu_name: str, project_id: str,
+def train_biomedbert(model_type: str, model_dir: str, pretraining_dir: str, bucket_name: str, tpu_name: str,
+                     project_id: str,
                      zone: str):
     """Method to train BioMedBERT"""
 
@@ -56,9 +57,6 @@ def train_biomedbert(model_type: str, model_dir: str, pretraining_dir: str, buck
     output_dir = 'gs://{}/{}'.format(bucket_name, model_dir)
     bert_base_dir = 'gs://{}/{}'.format(bucket_name, model_dir)
 
-    if init_checkpoint is None:
-        init_checkpoint = False
-
     # training procedure config
     train_batch_size = 256
     eval_batch_size = 256
@@ -70,121 +68,33 @@ def train_biomedbert(model_type: str, model_dir: str, pretraining_dir: str, buck
     num_tpu_cores = 128
 
     # train biomedbert
-    try:
-        run('python3 bert/run_pretraining.py   --input_file={}   --output_dir={}   --do_train=True   --do_eval=True   '
-            '--bert_config_file={}/{}   --init_checkpoint={}   --train_batch_size={}   --eval_batch_size={}   '
-            '--max_seq_length={}   --max_predictions_per_seq={}   --num_train_steps={}   --num_warmup_steps={}   '
-            '--learning_rate={}   --use_tpu=true   --tpu_name={}   --tpu_zone={}   --gcp_project={}   '
-            '--num_tpu_cores={}'.format(
-            input_file, output_dir, bert_base_dir, config, init_checkpoint, train_batch_size,
-            eval_batch_size, max_seq_length, max_predictions, train_steps, num_warmup_steps,
-            learning_rate, tpu_name, zone, project_id, num_tpu_cores
-        ))
-    except exceptions.UnexpectedExit:
-        log.info('Cannot train BioMedBERT')
-
-    # # use this for BERT-base
-    # bert_base_config = {
-    #     "attention_probs_dropout_prob": 0.1,
-    #     "directionality": "bidi",
-    #     "hidden_act": "gelu",
-    #     "hidden_dropout_prob": 0.1,
-    #     "hidden_size": 1024,
-    #     "initializer_range": 0.02,
-    #     "intermediate_size": 4096,
-    #     "max_position_embeddings": 512,
-    #     "num_attention_heads": 16,
-    #     "num_hidden_layers": 24,
-    #     "pooler_fc_size": 768,
-    #     "pooler_num_attention_heads": 12,
-    #     "pooler_num_fc_layers": 3,
-    #     "pooler_size_per_head": 128,
-    #     "pooler_type": "first_token_transform",
-    #     "type_vocab_size": 2,
-    #     "vocab_size": voc_size
-    # }
-
-    # with open("{}/bert_config.json".format(model_dir), "w") as fo:
-    #     json.dump(bert_base_config, fo, indent=2)
-
-    # if os.path.exists(model_dir):
-    #     try:
-    #         run('gsutil -m cp -r {} gs://{}'.format(model_dir, bucket_name))
-    #     except exceptions.UnexpectedExit:
-    #         print('Could not upload {} to GCS'.format(model_dir))
-
-    # # Input data pipeline config
-    # train_batch_size = 128
-    # max_predictions = 20
-    # max_seq_length = 128
-
-    # # Training procedure config
-    # eval_batch_size = 128
-    # learning_rate = 1e-5  # 2e-5
-    # train_steps = 1000000  # 1M
-    # save_checkpoints_steps = 25000  # 2500 -> 25K
-    # iterations_per_loop = 1000
-    # num_tpu_cores = 128
-    # num_warmup_steps = 10000
-
-    # if bucket_name:
-    #     bucket_path = "gs://{}".format(bucket_name)
-    # else:
-    #     bucket_path = "."
-
-    # bert_gcs_dir = "{}/{}".format(bucket_path, model_dir)
-    # data_gcs_dir = "{}/{}".format(bucket_path, pretraining_dir)
-
-    # config_file = os.path.join(bert_gcs_dir, "bert_config.json")
-
-    # init_checkpoint = tf.train.latest_checkpoint(bert_gcs_dir)
-
-    # bert_config = modeling.BertConfig.from_json_file(config_file)
-    # input_files = tf.io.gfile.glob(os.path.join(data_gcs_dir, '*tfrecord'))
-
-    # log.info("Using checkpoint: {}".format(init_checkpoint))
-    # log.info("Using {} data shards".format(len(input_files)))
-
-    # use_tpu = True
-
-    # model_fn = model_fn_builder(
-    #     bert_config=bert_config,
-    #     init_checkpoint=init_checkpoint,
-    #     learning_rate=learning_rate,
-    #     num_train_steps=train_steps,
-    #     num_warmup_steps=num_warmup_steps,
-    #     use_tpu=use_tpu,
-    #     use_one_hot_embeddings=use_tpu
-    # )
-
-    # tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-    #     zone=zone, project=project_id, job_name='biomedbert')#, tpu='for-shweta-tpu')
-
-    # is_per_host = tf.compat.v1.estimator.tpu.InputPipelineConfig.PER_HOST_V2
-    # run_config = tf.compat.v1.estimator.tpu.RunConfig(
-    #     cluster=tpu_cluster_resolver,
-    #     model_dir=bert_gcs_dir,
-    #     save_checkpoints_steps=save_checkpoints_steps,
-    #     tpu_config=tf.compat.v1.estimator.tpu.TPUConfig(
-    #         iterations_per_loop=iterations_per_loop,
-    #         num_shards=num_tpu_cores,
-    #         per_host_input_for_training=is_per_host))
-
-    # estimator = tf.compat.v1.estimator.tpu.TPUEstimator(
-    #     use_tpu=use_tpu,
-    #     model_fn=model_fn,
-    #     config=run_config,
-    #     train_batch_size=train_batch_size,
-    #     eval_batch_size=eval_batch_size)
-
-    # train_input_fn = input_fn_builder(
-    #     input_files=input_files,
-    #     max_seq_length=max_seq_length,
-    #     max_predictions_per_seq=max_predictions,
-    #     is_training=True,
-    #     num_cpu_threads=4)#64)
-
-    # estimator.train(input_fn=train_input_fn, max_steps=train_steps)
+    if init_checkpoint is None:
+        try:
+            run(
+                'python3 bert/run_pretraining.py   --input_file={}   --output_dir={}   --do_train=True   '
+                '--do_eval=True  --bert_config_file={}/{}   --train_batch_size={}   --eval_batch_size={}   '
+                '--max_seq_length={}   --max_predictions_per_seq={}   --num_train_steps={}   --num_warmup_steps={}   '
+                '--learning_rate={}   --use_tpu=true   --tpu_name={}   --tpu_zone={}   --gcp_project={}   '
+                '--num_tpu_cores={}'.format(
+                    input_file, output_dir, bert_base_dir, config, train_batch_size,
+                    eval_batch_size, max_seq_length, max_predictions, train_steps, num_warmup_steps,
+                    learning_rate, tpu_name, zone, project_id, num_tpu_cores
+                ))
+        except exceptions.UnexpectedExit:
+            log.info('Cannot train BioMedBERT')
+    else:
+        try:
+            run('python3 bert/run_pretraining.py   --input_file={}   --output_dir={}   --do_train=True   '
+                '--do_eval=True  --bert_config_file={}/{}   --init_checkpoint={}   --train_batch_size={}   '
+                '--eval_batch_size={}  --max_seq_length={}   --max_predictions_per_seq={}   --num_train_steps={}   '
+                '--num_warmup_steps={}  --learning_rate={}   --use_tpu=true   --tpu_name={}   --tpu_zone={}   '
+                '--gcp_project={}  --num_tpu_cores={}'.format(
+                input_file, output_dir, bert_base_dir, config, init_checkpoint, train_batch_size,
+                eval_batch_size, max_seq_length, max_predictions, train_steps, num_warmup_steps,
+                learning_rate, tpu_name, zone, project_id, num_tpu_cores
+            ))
+        except exceptions.UnexpectedExit:
+            log.info('Cannot train BioMedBERT')
 
 
 def train_vocabulary(data_path: str, prefix: str):
