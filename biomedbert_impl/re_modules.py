78f821bf868321089e3d251780f786cb7af98b5b
Ekaba Bisong <dvdbisong@gmail.com>
2020-05-24 02:42:46 -0600

    finetune re

--- biomedbert_impl/re_modules.py
+++ biomedbert_impl/re_modules.py
@@ -37,18 +37,13 @@ def fine_tune_re(re_dataset: str, re_dataset_no: str, model_dir: str, model_type
     output_dir = 'gs://{}/{}/RE_outputs/{}/{}'.format(bucket_name, model_dir, re_dataset, re_dataset_no)
     data_dir = 'gs://{}/datasets/RE/{}/{}'.format(bucket_name, re_dataset, re_dataset_no)
 
-    '''python run_re.py --task_name=$TASK_NAME --do_train=true --do_eval=true --do_predict=true 
-    --vocab_file=$BIOBERT_DIR/vocab.txt --bert_config_file=$BIOBERT_DIR/bert_config.json 
-    --init_checkpoint=$BIOBERT_DIR/model.ckpt-1000000 --max_seq_length=128 --train_batch_size=32 --learning_rate=2e-5 
-    --num_train_epochs=3.0 --do_lower_case=false --data_dir=$RE_DIR --output_dir=$OUTPUT_DIR '''
-
     try:
         run('python3 biobert/run_re.py  --task_name={}  --vocab_file={}   '
             '--bert_config_file={}  --init_checkpoint={}   --do_train=true --do_predict=true  '
-            '--max_seq_length=128   --train_batch_size=128   --learning_rate=2e-5   '
-            '--do_lower_case=false  --predict_batch_size=128  --num_train_epochs=10.0   --data_dir={}   '
-            '--output_dir={}  --num_tpu_cores=128   --use_tpu={}   '
-            '--tpu_name={}   --tpu_zone={}  --gcp_project={}   --num_tpu_cores={}'.format(
+            '--max_seq_length=128   --train_batch_size=32   --learning_rate=2e-5   '
+            '--do_lower_case=false  --predict_batch_size=32  --num_train_epochs=10.0   --data_dir={}   '
+            '--output_dir={}  --use_tpu={}  --tpu_name={}   --tpu_zone={}  '
+            '--gcp_project={}   --num_tpu_cores={}'.format(
             re_dataset, vocab_file, bert_config_file, init_checkpoint,
             data_dir, output_dir, use_tpu, tpu_name, tpu_zone, gcp_project, num_tpu_cores))
     except exceptions.UnexpectedExit:
