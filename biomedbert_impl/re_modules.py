93fd69d6fb59e5c04b5a1ac7668117643c4a8583
Ekaba Bisong <dvdbisong@gmail.com>
2020-05-24 09:57:46 -0600

    finetune ner/re

--- biomedbert_impl/re_modules.py
+++ biomedbert_impl/re_modules.py
@@ -34,13 +34,13 @@ def fine_tune_re(re_dataset: str, re_dataset_no: str, model_dir: str, model_type
     init_checkpoint = tf.train.latest_checkpoint('gs://{}/{}'.format(bucket_name, model_dir))
     vocab_file = 'gs://{}/{}/vocab.txt'.format(bucket_name, model_dir)
     bert_config_file = 'gs://{}/{}/{}'.format(bucket_name, model_dir, config)
-    output_dir = 'gs://{}/{}/RE_outputs/{}/{}'.format(bucket_name, model_dir, re_dataset, re_dataset_no)
-    data_dir = 'gs://{}/datasets/RE/{}/{}'.format(bucket_name, re_dataset, re_dataset_no)
+    output_dir = 'gs://{}/{}/RE_outputs/{}'.format(bucket_name, model_dir, re_dataset)  # , re_dataset_no)
+    data_dir = 'gs://{}/datasets/RE/{}'.format(bucket_name, re_dataset)  # , re_dataset_no)
 
     try:
         run('python3 biobert/run_re.py  --task_name={}  --vocab_file={}   '
             '--bert_config_file={}  --init_checkpoint={}   --do_train=true --do_predict=true  '
-            '--max_seq_length=128   --train_batch_size=32   --learning_rate=2e-5   '
+            '--max_seq_length=128   --train_batch_size=32   --learning_rate=1e-5   '
             '--do_lower_case=false  --predict_batch_size=32  --num_train_epochs=10.0   --data_dir={}   '
             '--output_dir={}  --use_tpu={}  --tpu_name={}   --tpu_zone={}  '
             '--gcp_project={}   --num_tpu_cores={}'.format(
@@ -52,11 +52,11 @@ def fine_tune_re(re_dataset: str, re_dataset_no: str, model_dir: str, model_type
 
 def evaluate_re(re_dataset: str, re_dataset_no: str, model_dir: str, bucket_name: str):
     """evaluate re"""
-    output_dir = 'gs://{}/{}/RE_outputs/{}/{}'.format(bucket_name, model_dir, re_dataset, re_dataset_no)
-    data_dir = 'gs://{}/datasets/RE/{}/{}'.format(bucket_name, re_dataset, re_dataset_no)
+    output_dir = 'gs://{}/{}/RE_outputs/{}'.format(bucket_name, model_dir, re_dataset)  # , re_dataset_no)
+    data_dir = 'gs://{}/datasets/RE/{}'.format(bucket_name, re_dataset)  # , re_dataset_no)
     try:
         run('python3 biobert/biocodes/re_eval.py --output_path={}/test_results.tsv --answer_path={}/test.tsv'.format(
             output_dir, data_dir
         ))
     except exceptions.UnexpectedExit:
-        print('Cannot evaluate RE - {}',format(re_dataset))
+        print('Cannot evaluate RE - {}', format(re_dataset))
