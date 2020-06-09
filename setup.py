868a2f84e5a29aafc2834f02752441231e0b9e82
Ekaba Bisong <dvdbisong@gmail.com>
2020-06-09 16:40:06 -0600

    pip install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 
 REQUIRES = [
     'docopt', 'invoke', 'tqdm', 'sentencepiece', 'google-api-python-client', 'oauth2client',
-    'tensorflow'
+    'tensorflow==1.15'
 ]
 
 
