2a11a75a4d582ddc0eef7fcec5301cb969ba8115
Ekaba Bisong <dvdbisong@gmail.com>
2020-07-07 05:46:20 -0600

    added dl4marco-bert

--- dl4marco-bert/run_msmarco.py
+++ dl4marco-bert/run_msmarco.py
@@ -201,7 +201,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
       train_op = optimization.create_optimizer(
           total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
 
-      output_spec = tf.compat.v1.contrib.tpu.TPUEstimatorSpec(
+      output_spec = tf.contrib.tpu.TPUEstimatorSpec(
           mode=mode,
           loss=total_loss,
           train_op=train_op,
